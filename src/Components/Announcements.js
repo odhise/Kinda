3eb61d266647283abf747f38a18a14dc2c420563
odhise <evansodhis491@gmail.com>
2022-04-01 15:36:29 +0300

    created navigation

---
+++ src/Components/Announcements.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Announcements=()=>{
+    return(
+        <div className="discounts">
+            <div>
+
+                <p>Email:Info@Kinda.co.ke</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Announcements;
---
+++ src/Components/Nav.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Nav=()=>{
+    return(
+        <div className="nav-container">
+             <div>
+                    <h1><span>Kinda</span><strong>.co.ke</strong></h1>
+                </div>
+            <div className="nav-content">
+                <div>
+                    <button className="btn1">BUY A CAR</button>
+                </div>
+                <div>
+                    <button className="btn2">SELL A CAR</button>
+                </div>
+                <div>
+                <button className="btn3">LOGIN</button>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Nav
